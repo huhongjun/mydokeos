4d148c182e30f0487f6c1d23822a6537cf793c6a
ZhouLei.bj <ZhouLei.bj@05bbc916-7450-0410-888c-3f026bcfbfc1>
2008-07-03 05:14:16 +0000

    dokeos本地化

--- mydokeos/main/lang/simpl_chinese/course_description.inc.php
+++ mydokeos/main/lang/simpl_chinese/course_description.inc.php
@@ -12,7 +12,7 @@ $langAddCat  = "添加分类";
 $langAdd  = "添加";
 $langValid  = "可用的";
 $langBackAndForget  = "返回和取消记忆";
-$CourseDescriptionUpdated = "&#35838;&#31243;&#25551;&#36848;&#26356;&#26032;&#23436;&#25104;";
-$CourseDescriptionDeleted = "&#35838;&#31243;&#25551;&#36848;&#21024;&#38500;&#23436;&#25104;";
-$CourseDescriptionIntro = "To create a course description, click on a heading and fill the correct form field.<br><br> Click then OK and fill another heading.";
+$CourseDescriptionUpdated = "课程描述已更新";
+$CourseDescriptionDeleted = "课程描述已删除";
+$CourseDescriptionIntro = "创建课程描述，点击标题并填写正确的表单字段。<br><br>然后点击确定并填写另一个标题。";
 ?>
