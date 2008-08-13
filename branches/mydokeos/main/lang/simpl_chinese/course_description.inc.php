df26eb96beefb4c229f0a40f0904da57e932482d
huhongjun <huhongjun@05bbc916-7450-0410-888c-3f026bcfbfc1>
2008-08-13 06:49:06 +0000

    为为国会客户建源码分支

---
+++ branches/mydokeos/main/lang/simpl_chinese/course_description.inc.php
@@ -0,0 +1,18 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langCourseProgram  = "课程简介";
+$langThisCourseDescriptionIsEmpty  = "此课程目前没有简介.";
+$langEditCourseProgram  = "创建和编辑课程简介";
+$QuestionPlan  = "给领导的问题";
+$langInfo2Say  = "发送给用户的信息";
+$langOuAutreTitre  = "标题";
+$langNewBloc  = "其他";
+$langAddCat  = "添加分类";
+$langAdd  = "添加";
+$langValid  = "可用的";
+$langBackAndForget  = "返回和取消记忆";
+$CourseDescriptionUpdated = "课程描述已更新";
+$CourseDescriptionDeleted = "课程描述已删除";
+$CourseDescriptionIntro = "创建课程描述，点击标题并填写正确的表单字段。<br><br>然后点击确定并填写另一个标题。";
+?>
---
+++ branches/mydokeos/main/lang/simpl_chinese/messages.inc.php
@@ -0,0 +1,24 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$Inbox  = "收件箱";
+$Messages = "消息";
+$SendMessage = "发送消息";
+$NewMessage = "新消息";
+$ComposeMessage = "撰写消息";
+$DeleteSelectedMessages = "删除选择消息";
+$SelectAll = "选择所有";
+$DeselectAll  = "取消所有";
+$ReplyToMessage = "回复";
+$BackToInbox = "返回收件箱";
+$MessageSentTo = "消息已发送";
+$SendMessageTo = "发送";
+$Myself = "我自己";
+$From = "从";
+$To = "到";
+$Date = "日期";
+$InvalidMessageId = "消息的id回复是无效的。";
+$ErrorSendingMessage = "当尝试发送消息时有一个错误。";
+$SureYouWantToDeleteSelectedMessages = "你确定你想删除选择的消息吗？";
+$SelectedMessagesDeleted = "选择的消息已删除";
+?>
