7c62c52526b68662bb01f0b0831c1edd3a6f1adf
zhangml9 <zhangml9@05bbc916-7450-0410-888c-3f026bcfbfc1>
2008-09-12 08:52:41 +0000

    "最后使用"页面显示"* 日 [60]"; 日 改成天数

--- dokeos/main/lang/simpl_chinese/admin.inc.php
+++ dokeos/main/lang/simpl_chinese/admin.inc.php
@@ -822,4 +822,14 @@ $cours = "课程";
 $Dates = "日期";
 $actions = "操作";
 $UsersAreSubscibedToCourse = "所选课程已注册到所选用户";
+
+$Mon = "星期一";
+$Tue = "星期二";
+$Wed = "星期三";
+$Thu = "星期四";
+$Fri = "星期五";
+$Sat = "星期六";
+$Sun = "星期天";
+
+$Days = "天数：";
 ?>
