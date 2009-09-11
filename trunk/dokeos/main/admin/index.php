c66d21b78be9f301e0e11e83978805604a205138
huhongjun@gmail.com <huhongjun@gmail.com@05bbc916-7450-0410-888c-3f026bcfbfc1>
2009-09-11 06:05:42 +0000

    issue 2 去掉管理员界面的Dokeos广告信息

--- trunk/dokeos/main/admin/index.php
+++ trunk/dokeos/main/admin/index.php
@@ -229,7 +229,9 @@ if(api_is_platform_admin()){
 	<!-- dynamic ldap code -->
 	<?php 
 	}
-?>
+	/*	
+	
+	?>
 <div class="admin_section">
  <h4>
   <img src="../img/dokeos.gif" border="0" style="vertical-align: middle;" alt="" />
@@ -245,7 +247,7 @@ if(api_is_platform_admin()){
   	session_write_close(); //close session to avoid blocking concurrent access
 	flush(); //send data to client as much as allowed by the web server
 	//ob_flush();
-	echo get_lang('VersionCheck').': '.version_check();
+	echo get_lang('VersionCheck').': '.version_check();*/
   ?>
   </li>
  </ul>
